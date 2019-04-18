e95f233f6a6f1ad958c3f0a05b2739599b27e94d
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-04-18 13:35:42 -0600

    removed all traces of session

--- server/server.js
+++ server/server.js
@@ -1,18 +1,9 @@
 const express = require('express')
-const session = require('express-session')
 const app = express()
 const ctrl = require('./controller')
 const SERVER_PORT = 4000
-const SECRET = 'aweofuhawegiUIjaw93893ghiou2p390ugiwj'
 
 app.use(express.json())
-app.use(
-  session({
-    secret: SECRET,
-    saveUninitialized: false,
-    resave: false
-  })
-)
 
 app.get('/messages', ctrl.getMessages)
 app.post('/message', ctrl.addMessage)
